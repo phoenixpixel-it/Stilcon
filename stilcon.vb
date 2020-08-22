06d718b77df7f5c47b05d5e9a079ebed2c2d0cad
realluke <49283004+lucapexe@users.noreply.github.com>
2020-08-22 10:05:20 +0200

    Add files via upload

--- stilcon.vb
+++ stilcon.vb
@@ -6,13 +6,19 @@ Public Class stilcon
         'st_apri.Click
         If ofd.ShowDialog = DialogResult.OK Then
             'true
-            Dim getzise As Image
+            Dim getinfo As Image
+
             sfd.FileName = Path.GetFileName(ofd.FileName) & "_stilicon"
             st_gb_ai_icon.Image = Icon.ExtractAssociatedIcon(ofd.FileName).ToBitmap
-            getzise = st_gb_ai_icon.Image
-            st_gb_ii_name.Text = "Nome origine icona: " & ofd.FileName
-            st_gb_ii_size.Text = "Dimensioni icona: " & getzise.Width & "x" & getzise.Height
-            st_gb_ii_type.Text = "Tipo icona: Bitmap"
+            getinfo = st_gb_ai_icon.Image
+
+            st_gb_ii_name_label.Text = "Nome origine icona: " & Split(ofd.FileName, "\").First & "\...\" & Path.GetFileName(ofd.FileName)
+            st_gb_ii_size_label.Text = "Dimensioni icona: " & getinfo.Width & "px; " & getinfo.Height & "px"
+            st_gb_ii_resolution_label.Text = "Risoluzione icona: " & getinfo.HorizontalResolution & "; " & getinfo.VerticalResolution
+            st_gb_ii_type_label.Text = "Formato icona: " & getinfo.RawFormat.Guid.ToString
+            st_gb_ii_truesize_label.Text = "Dimensioni fisiche: " & getinfo.PhysicalDimension.Width & "px; " & getinfo.PhysicalDimension.Height & "px"
+            st_gb_ii_px_label.Text = "Formato pixel: " & getinfo.PixelFormat
+            st_gb_ii_palette_label.Text = "Palette: " & getinfo.Palette.Flags
             st_gb_ruba.Enabled = True
         End If
     End Sub
