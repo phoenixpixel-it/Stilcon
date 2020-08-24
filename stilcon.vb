172b664976970b9b4138dcced319f5a30a0efb1e
realluke <49283004+lucapexe@users.noreply.github.com>
2020-08-24 12:12:48 +0200

    Add files via upload

--- stilcon.vb
+++ stilcon.vb
@@ -8,42 +8,54 @@ Public Class stilcon
         'MyBase.Load
         If Environment.GetCommandLineArgs().Count > 1 Then
             'true
-            args = Environment.GetCommandLineArgs()
+            Try
+                'try
+                args = Environment.GetCommandLineArgs()
 
-            Dim getinfo As Image
+                Dim getinfo As Image
 
-            Me.Text = "Stilcon 1.1 - " & Chr(34) & "Rubando icone" & Chr(34) & " - " & Path.GetFileName(args(1))
-            st_gb_ai_icon.Image = Icon.ExtractAssociatedIcon(args(1)).ToBitmap
-            getinfo = st_gb_ai_icon.Image
+                Me.Text = "Stilcon 1.1 - " & Chr(34) & "Rubando icone" & Chr(34) & " - " & Path.GetFileName(args(1))
+                st_gb_ai_icon.Image = Icon.ExtractAssociatedIcon(args(1)).ToBitmap
+                getinfo = st_gb_ai_icon.Image
 
-            st_gb_ii_name_label.Text = "Nome origine icona: " & Split(Path.GetDirectoryName(args(1)), "\").First & "\...\" & Path.GetFileName(args(1))
-            st_gb_ii_size_label.Text = "Dimensioni icona: " & getinfo.Width * getinfo.Height & "px"
-            st_gb_ii_size_w_label.Text = "Lunghezza icona: " & getinfo.Width & "px"
-            st_gb_ii_size_h_label.Text = "Altezza icona: " & getinfo.Height & "px"
-            st_gb_ii_resolution_label.Text = "Risoluzione icona: " & getinfo.HorizontalResolution * getinfo.VerticalResolution & "px"
-            st_gb_ii_resolution_w_label.Text = "Lunghezza icona: " & getinfo.HorizontalResolution & "px"
-            st_gb_ii_resolution_h_label.Text = "Altezza icona: " & getinfo.VerticalResolution & "px"
-            st_mn_panel_ruba.Enabled = True
-            st_mn_panel_chiudi.Enabled = True
+                st_gb_ii_name_label.Text = "Nome origine icona: " & Split(Path.GetDirectoryName(args(1)), "\").First & "\...\" & Path.GetFileName(args(1))
+                st_gb_ii_size_label.Text = "Dimensioni icona: " & getinfo.Width * getinfo.Height & "px"
+                st_gb_ii_size_w_label.Text = "Lunghezza icona: " & getinfo.Width & "px"
+                st_gb_ii_size_h_label.Text = "Altezza icona: " & getinfo.Height & "px"
+                st_gb_ii_resolution_label.Text = "Risoluzione icona: " & getinfo.HorizontalResolution * getinfo.VerticalResolution & "px"
+                st_gb_ii_resolution_w_label.Text = "Lunghezza icona: " & getinfo.HorizontalResolution & "px"
+                st_gb_ii_resolution_h_label.Text = "Altezza icona: " & getinfo.VerticalResolution & "px"
+                st_mn_panel_ruba.Enabled = True
+                st_mn_panel_chiudi.Enabled = True
+            Catch ex As Exception
+                'ex
+                MsgBox(ex.Message, MsgBoxStyle.Critical)
+            End Try
         Else
             'false
-            args = Environment.GetCommandLineArgs()
+            Try
+                'try
+                args = Environment.GetCommandLineArgs()
 
-            Dim getinfo As Image
+                Dim getinfo As Image
 
-            Me.Text = "Stilcon 1.1 - " & Chr(34) & "Rubando icone" & Chr(34) & " - " & "Stilcon.exe"
-            st_gb_ai_icon.Image = Icon.ExtractAssociatedIcon("Stilcon.exe").ToBitmap
-            getinfo = st_gb_ai_icon.Image
+                Me.Text = "Stilcon 1.1 - " & Chr(34) & "Rubando icone" & Chr(34) & " - " & "Stilcon 1.1.exe"
+                st_gb_ai_icon.Image = Icon.ExtractAssociatedIcon("Stilcon 1.1.exe").ToBitmap
+                getinfo = st_gb_ai_icon.Image
 
-            st_gb_ii_name_label.Text = "Nome origine icona: " & Split(Application.StartupPath, "\").First & "\...\" & Path.GetFileName("Stilcon.exe")
-            st_gb_ii_size_label.Text = "Dimensioni icona: " & getinfo.Width * getinfo.Height & "px"
-            st_gb_ii_size_w_label.Text = "Lunghezza icona: " & getinfo.Width & "px"
-            st_gb_ii_size_h_label.Text = "Altezza icona: " & getinfo.Height & "px"
-            st_gb_ii_resolution_label.Text = "Risoluzione icona: " & getinfo.HorizontalResolution * getinfo.VerticalResolution & "px"
-            st_gb_ii_resolution_w_label.Text = "Lunghezza icona: " & getinfo.HorizontalResolution & "px"
-            st_gb_ii_resolution_h_label.Text = "Altezza icona: " & getinfo.VerticalResolution & "px"
-            st_mn_panel_ruba.Enabled = True
-            st_mn_panel_chiudi.Enabled = True
+                st_gb_ii_name_label.Text = "Nome origine icona: " & Split(Application.StartupPath, "\").First & "\...\" & Path.GetFileName("Stilcon 1.1.exe")
+                st_gb_ii_size_label.Text = "Dimensioni icona: " & getinfo.Width * getinfo.Height & "px"
+                st_gb_ii_size_w_label.Text = "Lunghezza icona: " & getinfo.Width & "px"
+                st_gb_ii_size_h_label.Text = "Altezza icona: " & getinfo.Height & "px"
+                st_gb_ii_resolution_label.Text = "Risoluzione icona: " & getinfo.HorizontalResolution * getinfo.VerticalResolution & "px"
+                st_gb_ii_resolution_w_label.Text = "Lunghezza icona: " & getinfo.HorizontalResolution & "px"
+                st_gb_ii_resolution_h_label.Text = "Altezza icona: " & getinfo.VerticalResolution & "px"
+                st_mn_panel_ruba.Enabled = True
+                st_mn_panel_chiudi.Enabled = True
+            Catch ex As Exception
+                'ex
+                MsgBox(ex.Message, MsgBoxStyle.Critical)
+            End Try
         End If
     End Sub
 
@@ -51,22 +63,28 @@ Public Class stilcon
         'st_mn_panel_apri.Click
         If ofd.ShowDialog = DialogResult.OK Then
             'true
-            Dim getinfo As Image
+            Try
+                'try
+                Dim getinfo As Image
 
-            Me.Text = "Stilcon 1.1 - " & Chr(34) & "Rubando icone" & Chr(34) & " - " & ofd.SafeFileName
-            sfd.FileName = Path.GetFileName(ofd.FileName) & "_stilicon"
-            st_gb_ai_icon.Image = Icon.ExtractAssociatedIcon(ofd.FileName).ToBitmap
-            getinfo = st_gb_ai_icon.Image
+                Me.Text = "Stilcon 1.1 - " & Chr(34) & "Rubando icone" & Chr(34) & " - " & ofd.SafeFileName
+                sfd.FileName = Path.GetFileName(ofd.FileName) & "_stilicon"
+                st_gb_ai_icon.Image = Icon.ExtractAssociatedIcon(ofd.FileName).ToBitmap
+                getinfo = st_gb_ai_icon.Image
 
-            st_gb_ii_name_label.Text = "Nome origine icona: " & Split(ofd.FileName, "\").First & "\...\" & Path.GetFileName(ofd.FileName)
-            st_gb_ii_size_label.Text = "Dimensioni icona: " & getinfo.Width * getinfo.Height & "px"
-            st_gb_ii_size_w_label.Text = "Lunghezza icona: " & getinfo.Width & "px"
-            st_gb_ii_size_h_label.Text = "Altezza icona: " & getinfo.Height & "px"
-            st_gb_ii_resolution_label.Text = "Risoluzione icona: " & getinfo.HorizontalResolution * getinfo.VerticalResolution & "px"
-            st_gb_ii_resolution_w_label.Text = "Lunghezza icona: " & getinfo.HorizontalResolution & "px"
-            st_gb_ii_resolution_h_label.Text = "Altezza icona: " & getinfo.VerticalResolution & "px"
-            st_mn_panel_ruba.Enabled = True
-            st_mn_panel_chiudi.Enabled = True
+                st_gb_ii_name_label.Text = "Nome origine icona: " & Split(ofd.FileName, "\").First & "\...\" & Path.GetFileName(ofd.FileName)
+                st_gb_ii_size_label.Text = "Dimensioni icona: " & getinfo.Width * getinfo.Height & "px"
+                st_gb_ii_size_w_label.Text = "Lunghezza icona: " & getinfo.Width & "px"
+                st_gb_ii_size_h_label.Text = "Altezza icona: " & getinfo.Height & "px"
+                st_gb_ii_resolution_label.Text = "Risoluzione icona: " & getinfo.HorizontalResolution * getinfo.VerticalResolution & "px"
+                st_gb_ii_resolution_w_label.Text = "Lunghezza icona: " & getinfo.HorizontalResolution & "px"
+                st_gb_ii_resolution_h_label.Text = "Altezza icona: " & getinfo.VerticalResolution & "px"
+                st_mn_panel_ruba.Enabled = True
+                st_mn_panel_chiudi.Enabled = True
+            Catch ex As Exception
+                'ex
+                MsgBox(ex.Message, MsgBoxStyle.Critical)
+            End Try
         End If
     End Sub
 
@@ -91,8 +109,14 @@ Public Class stilcon
         'st_mn_panel_ruba.Click
         If sfd.ShowDialog = DialogResult.OK Then
             'true
-            st_gb_ai_icon.Image.Save(sfd.FileName)
-            Process.Start(Path.GetDirectoryName(sfd.FileName))
+            Try
+                'try
+                st_gb_ai_icon.Image.Save(sfd.FileName)
+                Process.Start(Path.GetDirectoryName(sfd.FileName))
+            Catch ex As Exception
+                'ex
+                MsgBox(ex.Message, MsgBoxStyle.Critical)
+            End Try
         End If
     End Sub
 
