8a3daa01d69b1b4b3ee7b25adb56e74f05bc357b
realluke <49283004+lucapexe@users.noreply.github.com>
2020-08-24 11:49:16 +0200

    Add files via upload

--- stilcon.Designer.vb
+++ stilcon.Designer.vb
@@ -22,25 +22,35 @@ Partial Class stilcon
     'Non modificarla mediante l'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(stilcon))
         Me.st_gb = New System.Windows.Forms.GroupBox()
         Me.st_gb_ai = New System.Windows.Forms.GroupBox()
+        Me.st_gb_ai_tb = New System.Windows.Forms.TrackBar()
         Me.st_gb_ai_icon = New System.Windows.Forms.PictureBox()
         Me.st_gb_ii = New System.Windows.Forms.GroupBox()
+        Me.st_gb_ii_resolution_h_label = New System.Windows.Forms.Label()
+        Me.st_gb_ii_resolution_w_label = New System.Windows.Forms.Label()
+        Me.st_gb_ii_size_h_label = New System.Windows.Forms.Label()
+        Me.st_gb_ii_size_w_label = New System.Windows.Forms.Label()
+        Me.st_gb_ii_resolution_label = New System.Windows.Forms.Label()
         Me.st_gb_ii_name_label = New System.Windows.Forms.Label()
-        Me.st_apri = New System.Windows.Forms.Button()
-        Me.st_gb_ii_type_label = New System.Windows.Forms.Label()
         Me.st_gb_ii_size_label = New System.Windows.Forms.Label()
-        Me.st_gb_ruba = New System.Windows.Forms.Button()
+        Me.st_mn_panel_apri = New System.Windows.Forms.Button()
+        Me.st_mn_panel_ruba = New System.Windows.Forms.Button()
         Me.ofd = New System.Windows.Forms.OpenFileDialog()
         Me.sfd = New System.Windows.Forms.SaveFileDialog()
-        Me.st_gb_ii_resolution_label = New System.Windows.Forms.Label()
-        Me.st_gb_ii_truesize_label = New System.Windows.Forms.Label()
-        Me.st_gb_ii_palette_label = New System.Windows.Forms.Label()
-        Me.st_gb_ii_px_label = New System.Windows.Forms.Label()
+        Me.st_ref_panel = New System.Windows.Forms.Panel()
+        Me.st_ref_panel_label = New System.Windows.Forms.Label()
+        Me.st_ref_panel_link = New System.Windows.Forms.LinkLabel()
+        Me.st_mn_panel_chiudi = New System.Windows.Forms.Button()
+        Me.st_mn = New System.Windows.Forms.Panel()
         Me.st_gb.SuspendLayout()
         Me.st_gb_ai.SuspendLayout()
+        CType(Me.st_gb_ai_tb, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.st_gb_ai_icon, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.st_gb_ii.SuspendLayout()
+        Me.st_ref_panel.SuspendLayout()
+        Me.st_mn.SuspendLayout()
         Me.SuspendLayout()
         '
         'st_gb
@@ -48,47 +58,103 @@ Partial Class stilcon
         Me.st_gb.Controls.Add(Me.st_gb_ai)
         Me.st_gb.Controls.Add(Me.st_gb_ii)
         Me.st_gb.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.st_gb.Location = New System.Drawing.Point(14, 12)
+        Me.st_gb.Location = New System.Drawing.Point(13, 56)
         Me.st_gb.Name = "st_gb"
-        Me.st_gb.Size = New System.Drawing.Size(615, 220)
+        Me.st_gb.Size = New System.Drawing.Size(636, 260)
         Me.st_gb.TabIndex = 0
         Me.st_gb.TabStop = False
         Me.st_gb.Text = "Stilcon"
         '
         'st_gb_ai
         '
+        Me.st_gb_ai.Controls.Add(Me.st_gb_ai_tb)
         Me.st_gb_ai.Controls.Add(Me.st_gb_ai_icon)
         Me.st_gb_ai.Location = New System.Drawing.Point(7, 26)
         Me.st_gb_ai.Name = "st_gb_ai"
-        Me.st_gb_ai.Size = New System.Drawing.Size(165, 185)
+        Me.st_gb_ai.Size = New System.Drawing.Size(165, 225)
         Me.st_gb_ai.TabIndex = 2
         Me.st_gb_ai.TabStop = False
         Me.st_gb_ai.Text = "Anteprima icona"
         '
+        'st_gb_ai_tb
+        '
+        Me.st_gb_ai_tb.Location = New System.Drawing.Point(7, 26)
+        Me.st_gb_ai_tb.Maximum = 2
+        Me.st_gb_ai_tb.Minimum = 1
+        Me.st_gb_ai_tb.Name = "st_gb_ai_tb"
+        Me.st_gb_ai_tb.Size = New System.Drawing.Size(150, 56)
+        Me.st_gb_ai_tb.TabIndex = 8
+        Me.st_gb_ai_tb.Value = 1
+        '
         'st_gb_ai_icon
         '
-        Me.st_gb_ai_icon.Location = New System.Drawing.Point(7, 26)
+        Me.st_gb_ai_icon.Location = New System.Drawing.Point(7, 88)
         Me.st_gb_ai_icon.Name = "st_gb_ai_icon"
-        Me.st_gb_ai_icon.Size = New System.Drawing.Size(150, 150)
+        Me.st_gb_ai_icon.Size = New System.Drawing.Size(150, 125)
         Me.st_gb_ai_icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
         Me.st_gb_ai_icon.TabIndex = 0
         Me.st_gb_ai_icon.TabStop = False
         '
         'st_gb_ii
         '
-        Me.st_gb_ii.Controls.Add(Me.st_gb_ii_px_label)
+        Me.st_gb_ii.Controls.Add(Me.st_gb_ii_resolution_h_label)
+        Me.st_gb_ii.Controls.Add(Me.st_gb_ii_resolution_w_label)
+        Me.st_gb_ii.Controls.Add(Me.st_gb_ii_size_h_label)
+        Me.st_gb_ii.Controls.Add(Me.st_gb_ii_size_w_label)
         Me.st_gb_ii.Controls.Add(Me.st_gb_ii_resolution_label)
-        Me.st_gb_ii.Controls.Add(Me.st_gb_ii_truesize_label)
-        Me.st_gb_ii.Controls.Add(Me.st_gb_ii_palette_label)
         Me.st_gb_ii.Controls.Add(Me.st_gb_ii_name_label)
-        Me.st_gb_ii.Controls.Add(Me.st_gb_ii_type_label)
         Me.st_gb_ii.Controls.Add(Me.st_gb_ii_size_label)
         Me.st_gb_ii.Location = New System.Drawing.Point(178, 26)
         Me.st_gb_ii.Name = "st_gb_ii"
-        Me.st_gb_ii.Size = New System.Drawing.Size(425, 185)
+        Me.st_gb_ii.Size = New System.Drawing.Size(450, 225)
         Me.st_gb_ii.TabIndex = 1
         Me.st_gb_ii.TabStop = False
         Me.st_gb_ii.Text = "Informazioni icona"
+        '
+        'st_gb_ii_resolution_h_label
+        '
+        Me.st_gb_ii_resolution_h_label.AutoSize = True
+        Me.st_gb_ii_resolution_h_label.Location = New System.Drawing.Point(45, 142)
+        Me.st_gb_ii_resolution_h_label.Name = "st_gb_ii_resolution_h_label"
+        Me.st_gb_ii_resolution_h_label.Size = New System.Drawing.Size(89, 20)
+        Me.st_gb_ii_resolution_h_label.TabIndex = 11
+        Me.st_gb_ii_resolution_h_label.Text = "Altezza: n/A"
+        '
+        'st_gb_ii_resolution_w_label
+        '
+        Me.st_gb_ii_resolution_w_label.AutoSize = True
+        Me.st_gb_ii_resolution_w_label.Location = New System.Drawing.Point(45, 122)
+        Me.st_gb_ii_resolution_w_label.Name = "st_gb_ii_resolution_w_label"
+        Me.st_gb_ii_resolution_w_label.Size = New System.Drawing.Size(110, 20)
+        Me.st_gb_ii_resolution_w_label.TabIndex = 10
+        Me.st_gb_ii_resolution_w_label.Text = "Lunghezza: n/A"
+        '
+        'st_gb_ii_size_h_label
+        '
+        Me.st_gb_ii_size_h_label.AutoSize = True
+        Me.st_gb_ii_size_h_label.Location = New System.Drawing.Point(45, 82)
+        Me.st_gb_ii_size_h_label.Name = "st_gb_ii_size_h_label"
+        Me.st_gb_ii_size_h_label.Size = New System.Drawing.Size(89, 20)
+        Me.st_gb_ii_size_h_label.TabIndex = 9
+        Me.st_gb_ii_size_h_label.Text = "Altezza: n/A"
+        '
+        'st_gb_ii_size_w_label
+        '
+        Me.st_gb_ii_size_w_label.AutoSize = True
+        Me.st_gb_ii_size_w_label.Location = New System.Drawing.Point(45, 63)
+        Me.st_gb_ii_size_w_label.Name = "st_gb_ii_size_w_label"
+        Me.st_gb_ii_size_w_label.Size = New System.Drawing.Size(110, 20)
+        Me.st_gb_ii_size_w_label.TabIndex = 8
+        Me.st_gb_ii_size_w_label.Text = "Lunghezza: n/A"
+        '
+        'st_gb_ii_resolution_label
+        '
+        Me.st_gb_ii_resolution_label.AutoSize = True
+        Me.st_gb_ii_resolution_label.Location = New System.Drawing.Point(6, 102)
+        Me.st_gb_ii_resolution_label.Name = "st_gb_ii_resolution_label"
+        Me.st_gb_ii_resolution_label.Size = New System.Drawing.Size(116, 20)
+        Me.st_gb_ii_resolution_label.TabIndex = 4
+        Me.st_gb_ii_resolution_label.Text = "Risoluzione: n/A"
         '
         'st_gb_ii_name_label
         '
@@ -99,25 +165,6 @@ Partial Class stilcon
         Me.st_gb_ii_name_label.TabIndex = 1
         Me.st_gb_ii_name_label.Text = "Nome origine icona: n/A"
         '
-        'st_apri
-        '
-        Me.st_apri.Font = New System.Drawing.Font("Segoe UI", 9.0!)
-        Me.st_apri.Location = New System.Drawing.Point(14, 238)
-        Me.st_apri.Name = "st_apri"
-        Me.st_apri.Size = New System.Drawing.Size(172, 40)
-        Me.st_apri.TabIndex = 1
-        Me.st_apri.Text = "Apri"
-        Me.st_apri.UseVisualStyleBackColor = True
-        '
-        'st_gb_ii_type_label
-        '
-        Me.st_gb_ii_type_label.AutoSize = True
-        Me.st_gb_ii_type_label.Location = New System.Drawing.Point(6, 83)
-        Me.st_gb_ii_type_label.Name = "st_gb_ii_type_label"
-        Me.st_gb_ii_type_label.Size = New System.Drawing.Size(96, 20)
-        Me.st_gb_ii_type_label.TabIndex = 2
-        Me.st_gb_ii_type_label.Text = "Formato: n/A"
-        '
         'st_gb_ii_size_label
         '
         Me.st_gb_ii_size_label.AutoSize = True
@@ -127,16 +174,26 @@ Partial Class stilcon
         Me.st_gb_ii_size_label.TabIndex = 3
         Me.st_gb_ii_size_label.Text = "Dimensioni: n/A"
         '
-        'st_gb_ruba
+        'st_mn_panel_apri
         '
-        Me.st_gb_ruba.Enabled = False
-        Me.st_gb_ruba.Font = New System.Drawing.Font("Segoe UI", 9.0!)
-        Me.st_gb_ruba.Location = New System.Drawing.Point(192, 238)
-        Me.st_gb_ruba.Name = "st_gb_ruba"
-        Me.st_gb_ruba.Size = New System.Drawing.Size(437, 40)
-        Me.st_gb_ruba.TabIndex = 2
-        Me.st_gb_ruba.Text = "Ruba"
-        Me.st_gb_ruba.UseVisualStyleBackColor = True
+        Me.st_mn_panel_apri.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.st_mn_panel_apri.Location = New System.Drawing.Point(12, 10)
+        Me.st_mn_panel_apri.Name = "st_mn_panel_apri"
+        Me.st_mn_panel_apri.Size = New System.Drawing.Size(100, 30)
+        Me.st_mn_panel_apri.TabIndex = 1
+        Me.st_mn_panel_apri.Text = "Apri"
+        Me.st_mn_panel_apri.UseVisualStyleBackColor = True
+        '
+        'st_mn_panel_ruba
+        '
+        Me.st_mn_panel_ruba.Enabled = False
+        Me.st_mn_panel_ruba.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.st_mn_panel_ruba.Location = New System.Drawing.Point(225, 10)
+        Me.st_mn_panel_ruba.Name = "st_mn_panel_ruba"
+        Me.st_mn_panel_ruba.Size = New System.Drawing.Size(425, 30)
+        Me.st_mn_panel_ruba.TabIndex = 2
+        Me.st_mn_panel_ruba.Text = "Ruba"
+        Me.st_mn_panel_ruba.UseVisualStyleBackColor = True
         '
         'ofd
         '
@@ -151,63 +208,85 @@ Partial Class stilcon
     "*.jfi|File ICO|*.ico|File CUR|*.cur|Tutti i file|*.*"
         Me.sfd.Title = "Ruba"
         '
-        'st_gb_ii_resolution_label
+        'st_ref_panel
         '
-        Me.st_gb_ii_resolution_label.AutoSize = True
-        Me.st_gb_ii_resolution_label.Location = New System.Drawing.Point(6, 63)
-        Me.st_gb_ii_resolution_label.Name = "st_gb_ii_resolution_label"
-        Me.st_gb_ii_resolution_label.Size = New System.Drawing.Size(116, 20)
-        Me.st_gb_ii_resolution_label.TabIndex = 4
-        Me.st_gb_ii_resolution_label.Text = "Risoluzione: n/A"
+        Me.st_ref_panel.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.st_ref_panel.Controls.Add(Me.st_ref_panel_label)
+        Me.st_ref_panel.Controls.Add(Me.st_ref_panel_link)
+        Me.st_ref_panel.Dock = System.Windows.Forms.DockStyle.Top
+        Me.st_ref_panel.Location = New System.Drawing.Point(0, 0)
+        Me.st_ref_panel.Name = "st_ref_panel"
+        Me.st_ref_panel.Size = New System.Drawing.Size(662, 50)
+        Me.st_ref_panel.TabIndex = 3
         '
-        'st_gb_ii_truesize_label
+        'st_ref_panel_label
         '
-        Me.st_gb_ii_truesize_label.AutoSize = True
-        Me.st_gb_ii_truesize_label.Location = New System.Drawing.Point(6, 103)
-        Me.st_gb_ii_truesize_label.Name = "st_gb_ii_truesize_label"
-        Me.st_gb_ii_truesize_label.Size = New System.Drawing.Size(161, 20)
-        Me.st_gb_ii_truesize_label.TabIndex = 5
-        Me.st_gb_ii_truesize_label.Text = "Dimensioni fisiche: n/A"
+        Me.st_ref_panel_label.AutoSize = True
+        Me.st_ref_panel_label.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.st_ref_panel_label.Location = New System.Drawing.Point(10, 14)
+        Me.st_ref_panel_label.Name = "st_ref_panel_label"
+        Me.st_ref_panel_label.Size = New System.Drawing.Size(89, 23)
+        Me.st_ref_panel_label.TabIndex = 1
+        Me.st_ref_panel_label.Text = "Creato da:"
         '
-        'st_gb_ii_palette_label
+        'st_ref_panel_link
         '
-        Me.st_gb_ii_palette_label.AutoSize = True
-        Me.st_gb_ii_palette_label.Location = New System.Drawing.Point(6, 143)
-        Me.st_gb_ii_palette_label.Name = "st_gb_ii_palette_label"
-        Me.st_gb_ii_palette_label.Size = New System.Drawing.Size(85, 20)
-        Me.st_gb_ii_palette_label.TabIndex = 6
-        Me.st_gb_ii_palette_label.Text = "Palette: n/A"
+        Me.st_ref_panel_link.AutoSize = True
+        Me.st_ref_panel_link.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.st_ref_panel_link.Location = New System.Drawing.Point(102, 14)
+        Me.st_ref_panel_link.Name = "st_ref_panel_link"
+        Me.st_ref_panel_link.Size = New System.Drawing.Size(111, 23)
+        Me.st_ref_panel_link.TabIndex = 0
+        Me.st_ref_panel_link.TabStop = True
+        Me.st_ref_panel_link.Text = "Phoenix Pixel"
         '
-        'st_gb_ii_px_label
+        'st_mn_panel_chiudi
         '
-        Me.st_gb_ii_px_label.AutoSize = True
-        Me.st_gb_ii_px_label.Location = New System.Drawing.Point(6, 123)
-        Me.st_gb_ii_px_label.Name = "st_gb_ii_px_label"
-        Me.st_gb_ii_px_label.Size = New System.Drawing.Size(132, 20)
-        Me.st_gb_ii_px_label.TabIndex = 7
-        Me.st_gb_ii_px_label.Text = "Formato pixel: n/A"
+        Me.st_mn_panel_chiudi.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.st_mn_panel_chiudi.Location = New System.Drawing.Point(119, 10)
+        Me.st_mn_panel_chiudi.Name = "st_mn_panel_chiudi"
+        Me.st_mn_panel_chiudi.Size = New System.Drawing.Size(100, 30)
+        Me.st_mn_panel_chiudi.TabIndex = 4
+        Me.st_mn_panel_chiudi.Text = "Chiudi"
+        Me.st_mn_panel_chiudi.UseVisualStyleBackColor = True
+        '
+        'st_mn
+        '
+        Me.st_mn.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.st_mn.Controls.Add(Me.st_mn_panel_apri)
+        Me.st_mn.Controls.Add(Me.st_mn_panel_chiudi)
+        Me.st_mn.Controls.Add(Me.st_mn_panel_ruba)
+        Me.st_mn.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.st_mn.Location = New System.Drawing.Point(0, 335)
+        Me.st_mn.Name = "st_mn"
+        Me.st_mn.Size = New System.Drawing.Size(662, 50)
+        Me.st_mn.TabIndex = 5
         '
         'stilcon
         '
+        Me.AcceptButton = Me.st_mn_panel_apri
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(642, 293)
-        Me.Controls.Add(Me.st_gb_ruba)
-        Me.Controls.Add(Me.st_apri)
+        Me.ClientSize = New System.Drawing.Size(662, 385)
+        Me.Controls.Add(Me.st_mn)
+        Me.Controls.Add(Me.st_ref_panel)
         Me.Controls.Add(Me.st_gb)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
-        Me.MaximumSize = New System.Drawing.Size(660, 340)
-        Me.MinimizeBox = False
-        Me.MinimumSize = New System.Drawing.Size(660, 340)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
         Me.Name = "stilcon"
-        Me.ShowIcon = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Stilicon"
         Me.st_gb.ResumeLayout(False)
         Me.st_gb_ai.ResumeLayout(False)
+        Me.st_gb_ai.PerformLayout()
+        CType(Me.st_gb_ai_tb, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.st_gb_ai_icon, System.ComponentModel.ISupportInitialize).EndInit()
         Me.st_gb_ii.ResumeLayout(False)
         Me.st_gb_ii.PerformLayout()
+        Me.st_ref_panel.ResumeLayout(False)
+        Me.st_ref_panel.PerformLayout()
+        Me.st_mn.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -217,14 +296,20 @@ Partial Class stilcon
     Friend WithEvents st_gb_ai As GroupBox
     Friend WithEvents st_gb_ii As GroupBox
     Friend WithEvents st_gb_ii_name_label As Label
-    Friend WithEvents st_gb_ii_type_label As Label
     Friend WithEvents st_gb_ii_size_label As Label
-    Friend WithEvents st_apri As Button
-    Friend WithEvents st_gb_ruba As Button
+    Friend WithEvents st_mn_panel_apri As Button
+    Friend WithEvents st_mn_panel_ruba As Button
     Friend WithEvents ofd As OpenFileDialog
     Friend WithEvents sfd As SaveFileDialog
     Friend WithEvents st_gb_ii_resolution_label As Label
-    Friend WithEvents st_gb_ii_truesize_label As Label
-    Friend WithEvents st_gb_ii_palette_label As Label
-    Friend WithEvents st_gb_ii_px_label As Label
+    Friend WithEvents st_ref_panel As Panel
+    Friend WithEvents st_ref_panel_label As Label
+    Friend WithEvents st_ref_panel_link As LinkLabel
+    Friend WithEvents st_mn_panel_chiudi As Button
+    Friend WithEvents st_gb_ai_tb As TrackBar
+    Friend WithEvents st_mn As Panel
+    Friend WithEvents st_gb_ii_size_h_label As Label
+    Friend WithEvents st_gb_ii_size_w_label As Label
+    Friend WithEvents st_gb_ii_resolution_h_label As Label
+    Friend WithEvents st_gb_ii_resolution_w_label As Label
 End Class
--- stilcon.vb
+++ stilcon.vb
@@ -2,8 +2,26 @@
 
 Public Class stilcon
 
-    Private Sub st_apri_Click(sender As Object, e As EventArgs) Handles st_apri.Click
-        'st_apri.Click
+    Private Sub stilcon_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'MyBase.Load
+        Dim getinfo As Image
+
+        st_gb_ai_icon.Image = Icon.ExtractAssociatedIcon("Stilcon.exe").ToBitmap
+        getinfo = st_gb_ai_icon.Image
+
+        st_gb_ii_name_label.Text = "Nome origine icona: " & Split(Application.StartupPath, "\").First & "\...\" & Path.GetFileName("Stilcon.exe")
+        st_gb_ii_size_label.Text = "Dimensioni icona: " & getinfo.Width * getinfo.Height & "px"
+        st_gb_ii_size_w_label.Text = "Lunghezza icona: " & getinfo.Width & "px"
+        st_gb_ii_size_h_label.Text = "Altezza icona: " & getinfo.Height & "px"
+        st_gb_ii_resolution_label.Text = "Risoluzione icona: " & getinfo.HorizontalResolution * getinfo.VerticalResolution & "px"
+        st_gb_ii_resolution_w_label.Text = "Lunghezza icona: " & getinfo.HorizontalResolution & "px"
+        st_gb_ii_resolution_h_label.Text = "Altezza icona: " & getinfo.VerticalResolution & "px"
+        st_mn_panel_ruba.Enabled = True
+        st_mn_panel_chiudi.Enabled = True
+    End Sub
+
+    Private Sub st_mn_panel_apri_Click(sender As Object, e As EventArgs) Handles st_mn_panel_apri.Click
+        'st_mn_panel_apri.Click
         If ofd.ShowDialog = DialogResult.OK Then
             'true
             Dim getinfo As Image
@@ -13,22 +31,55 @@ Public Class stilcon
             getinfo = st_gb_ai_icon.Image
 
             st_gb_ii_name_label.Text = "Nome origine icona: " & Split(ofd.FileName, "\").First & "\...\" & Path.GetFileName(ofd.FileName)
-            st_gb_ii_size_label.Text = "Dimensioni icona: " & getinfo.Width & "px; " & getinfo.Height & "px"
-            st_gb_ii_resolution_label.Text = "Risoluzione icona: " & getinfo.HorizontalResolution & "; " & getinfo.VerticalResolution
-            st_gb_ii_type_label.Text = "Formato icona: " & getinfo.RawFormat.Guid.ToString
-            st_gb_ii_truesize_label.Text = "Dimensioni fisiche: " & getinfo.PhysicalDimension.Width & "px; " & getinfo.PhysicalDimension.Height & "px"
-            st_gb_ii_px_label.Text = "Formato pixel: " & getinfo.PixelFormat
-            st_gb_ii_palette_label.Text = "Palette: " & getinfo.Palette.Flags
-            st_gb_ruba.Enabled = True
+            st_gb_ii_size_label.Text = "Dimensioni icona: " & getinfo.Width * getinfo.Height & "px"
+            st_gb_ii_size_w_label.Text = "Lunghezza icona: " & getinfo.Width & "px"
+            st_gb_ii_size_h_label.Text = "Altezza icona: " & getinfo.Height & "px"
+            st_gb_ii_resolution_label.Text = "Risoluzione icona: " & getinfo.HorizontalResolution * getinfo.VerticalResolution & "px"
+            st_gb_ii_resolution_w_label.Text = "Lunghezza icona: " & getinfo.HorizontalResolution & "px"
+            st_gb_ii_resolution_h_label.Text = "Altezza icona: " & getinfo.VerticalResolution & "px"
+            st_mn_panel_ruba.Enabled = True
+            st_mn_panel_chiudi.Enabled = True
         End If
     End Sub
 
-    Private Sub st_gb_ruba_Click(sender As Object, e As EventArgs) Handles st_gb_ruba.Click
-        'st_gb_ruba.Click
+
+    Private Sub st_mn_panel_chiudi_Click(sender As Object, e As EventArgs) Handles st_mn_panel_chiudi.Click
+        'st_mn_panel_chiudi.Click
+        st_gb_ai_icon.Image = Nothing
+
+        st_gb_ii_name_label.Text = "Nome origine icona: " & "n/A"
+        st_gb_ii_size_label.Text = "Dimensioni icona: " & "n/A"
+        st_gb_ii_resolution_label.Text = "Risoluzione icona: " & "n/A"
+        st_gb_ii_size_w_label.Text = "Lunghezza icona: " & "n/A"
+        st_gb_ii_size_h_label.Text = "Altezza icona: " & "n/A"
+        st_gb_ii_resolution_w_label.Text = "Lunghezza icona: " & "n/A"
+        st_gb_ii_resolution_h_label.Text = "Altezza icona: " & "n/A"
+        st_mn_panel_ruba.Enabled = False
+        st_mn_panel_chiudi.Enabled = False
+    End Sub
+
+    Private Sub st_mn_panel_Click(sender As Object, e As EventArgs) Handles st_mn_panel_ruba.Click
+        'st_mn_panel_ruba.Click
         If sfd.ShowDialog = DialogResult.OK Then
             'true
             st_gb_ai_icon.Image.Save(sfd.FileName)
             Process.Start(Path.GetDirectoryName(sfd.FileName))
+        End If
+    End Sub
+
+    Private Sub st_ref_panel_link_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles st_ref_panel_link.LinkClicked
+        'st_ref_panel_link.LinkClicked
+        Process.Start("https://phoenixpixel.it/")
+    End Sub
+
+    Private Sub st_gb_ai_tb_Scroll(sender As Object, e As EventArgs) Handles st_gb_ai_tb.Scroll
+        'st_gb_ai_tb.Scroll
+        If st_gb_ai_tb.Value = 1 Then
+            'true
+            st_gb_ai_icon.SizeMode = PictureBoxSizeMode.CenterImage
+        Else
+            'false
+            st_gb_ai_icon.SizeMode = PictureBoxSizeMode.Zoom
         End If
     End Sub
 
